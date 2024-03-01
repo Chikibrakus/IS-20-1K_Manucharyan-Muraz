0d9f99151448491cceea148bc4f2bdc6a0942403
Chmoni <gobrogame@gmail.com>
2024-03-01 22:06:19 +0500

    Добавьте файлы проекта.

---
+++ Test/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop;
+using Microsoft.Office.Interop.Word;
+using System.IO;
+
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вы хотите создать чек?");
+            String Otvet = Console.ReadLine();
+            if (Otvet == "Да") 
+            {
+                Application application = new Application();
+                var document = application.Documents.Add();
+                document.Content.Text = "TEST";
+                document.SaveAs2(Path.GetFullPath($@"..\..\..\Чеки\{DateTime.Now.ToString("yyyyMMdd_HH mm ss")}.docx"));
+                document.Close();
+                application.Quit();
+            }
+            else if( Otvet == "Нет")
+            {
+                Console.WriteLine("Ладно.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
